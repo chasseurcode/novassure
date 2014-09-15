09670d277f9228cd67fa8c20219db2b51b255a30
taram <tsayouba@gmail.com>
2014-09-15 03:57:43 +0100

    -corection des bugs des dependences -ajout de templates

---
+++ src/main/java/ma/novassure/beans/Dashbord.java
@@ -0,0 +1,37 @@
+package ma.novassure.beans;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+import org.primefaces.model.DashboardColumn;
+import org.primefaces.model.DashboardModel;
+import org.primefaces.model.DefaultDashboardColumn;
+import org.primefaces.model.DefaultDashboardModel;
+@ManagedBean
+@ApplicationScoped
+public class Dashbord {
+	private DashboardModel model;
+	
+	@PostConstruct
+	public void initialiser() {
+		model=new DefaultDashboardModel();
+		DashboardColumn colonne1=new DefaultDashboardColumn();
+		DashboardColumn colonne2=new DefaultDashboardColumn();
+		
+		colonne1.addWidget("stat");
+		colonne1.addWidget("tbl_de_bord");
+		
+		model.addColumn(colonne1);
+	}
+
+	public DashboardModel getModel() {
+		return model;
+	}
+
+	public void setModel(DashboardModel model) {
+		this.model = model;
+	}
+	
+	
+}
