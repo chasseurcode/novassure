1dc570c2e608f79983348561a46c6c28919a44af
CAPTAIN CODE <CAPTAIN CODE@CAPTAINCODE-PC>
2014-10-13 04:22:52 +0100

    writing client gui

--- src/main/webapp/resources/js/novassure.js
+++ src/main/webapp/resources/js/novassure.js
@@ -3,3 +3,19 @@ $(document).ready(function(){
   $(".ui-panelmenu-header").addClass("ui-state-active"); //sets the submenu header to active state
   $(".ui-icon-triangle-1-e").removeClass("ui-icon-triangle-1-e").addClass("ui-icon-triangle-1-s"); //sets the triangle icon to "expaned" version
 });
+$(document).ready(function(){
+	$("#tableEntrepriseContainer").hide();
+	$("#formEntreprise").hide();	
+	$("#partic").click(function(){		
+		$("#formParticulier").show();
+		$("#tableParticulierContainer").show();
+		$("#formEntreprise").hide();
+		$("#tableEntrepriseContainer").hide();		
+	});
+	$("#entrep").click(function(){		
+		$("#formParticulier").hide()
+		$("#tableParticulierContainer").hide();
+		$("#formEntreprise").show();
+		$("#tableEntrepriseContainer").show();		
+	});
+});
