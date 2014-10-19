7e4239393ae02f69b3d8712bc742d326e288dff7
taram <tsayouba@gmail.com>
2014-10-19 06:59:20 +0100

    maj utilisateur

--- src/main/java/ma/novassure/beans/UserBean.java
+++ src/main/java/ma/novassure/beans/UserBean.java
@@ -3,7 +3,6 @@ package ma.novassure.beans;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 
 import ma.novassure.dao.RoleDAO;
